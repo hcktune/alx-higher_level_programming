cedfd4710c85e6f0124e7355fccba8b7373af771
aymanemontassir <aymane-hana@hotmail.fr>
2023-11-28 21:04:36 +0100

    remove exit()

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10,10)
+number = random.randint(-10, 10)
 if number > 0:
     print(f"{number} is positive")
 elif number < 0:
-    print(f"{numner} is negative")
+    print(f"{number} is negative")
 else:
-    print(f"{numner} is zero")
+    print(f"{number} is zero")

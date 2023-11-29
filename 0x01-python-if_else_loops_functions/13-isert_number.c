010d8331d172ef951614c0245b2e891b0c6e8705
aymanemontassir <aymane-hana@hotmail.fr>
2023-11-29 02:50:47 +0100

    add c file

---
+++ 0x01-python-if_else_loops_functions/13-isert_number.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a number into sing_list
+ * @head: the head
+ * @number: data
+ * Return: the address of the new node, otherwise NULL
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *tmp;
+
+	if (tmp == NULL || head == NULL)
+		return (NULL);
+
+	tmp = malloc(sizeof(listint_t));
+	tmp->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		tmp-next = node;
+		*head = tmp;
+		return (tmp);
+	}
+	 while (node && node->next && node->next->n < number)
+                node = node->next;
+
+        tmp->next = node->next;
+        node->next = tmp;
+
+        return (tmp);
+}

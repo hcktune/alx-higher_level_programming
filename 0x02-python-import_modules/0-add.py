e1319eb09170ed9e84f9f5b9be7277f396503952
aymanemontassir <aymane-hana@hotmail.fr>
2023-11-30 20:47:55 +0100

    we connot have variable contains the result of a + b

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,5 +4,4 @@ if __name__ == "__main__":
     from add_0 import add as add_func 
     a = 1
     b = 2
-    res = add_func(a, b)
-    print(f"{a} + {b} = {res}")
+    print("{} + {} = {}".format(a, b, add_func(a, b)))

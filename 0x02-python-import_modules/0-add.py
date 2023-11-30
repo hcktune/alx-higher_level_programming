8ae511a84e2aafa9057691af05375574f475348f
aymanemontassir <aymane-hana@hotmail.fr>
2023-11-30 20:49:08 +0100

    trailing space fixed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from add_0 import add as add_func 
+    from add_0 import add as add_func
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add_func(a, b)))

f126b3ffdca3b93bb2be81a6e58626fdd574df8c
aymanemontassir <aymane-hana@hotmail.fr>
2023-12-03 00:57:43 +0100

    just return function not print

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,5 +4,4 @@ def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list) - 1:
         return (None)
     else:
-        print("Element at index {:d} is {}".format(idx, my_list[idx]))
-        return
+        return (my_list[idx])

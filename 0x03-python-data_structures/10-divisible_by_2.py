2ec10081e35f05c497a7fd54b175633bd0753ad0
aymanemontassir <aymane-hana@hotmail.fr>
2023-12-04 03:31:31 +0100

    cleann the ****

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,6 +3,6 @@
 def divisible_by_2(my_list=[]):
     res = []
     for num in my_list:
-       divisible_by_2 = num % 2 == 0
-       res.append(divisible_by_2)
+        divisible_by_2 = num % 2 == 0
+        res.append(divisible_by_2)
     return res

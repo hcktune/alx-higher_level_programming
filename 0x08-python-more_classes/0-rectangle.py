da8cc0575c626f0296908381493237a8db95e2c8
aymanemontassir <aymane-hana@hotmail.fr>
2024-01-04 21:22:29 +0100

    correct : #!/usr/bin/python3

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,9 @@
-#!/usr/python3
+#!/usr/bin/python3
 
 """ empty class rectangel that defines a rectange """ 
 
 class Rectangle:
+        def __init__:
         """ represent a Rectrangle """
         pass
 

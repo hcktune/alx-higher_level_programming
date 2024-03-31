65bb09378b04cc404cdfe02494bf84a938156310
root <mohammed.aymane.montassir@gmail.com>
2024-03-31 06:22:05 -0700

    remove comments

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,13 +3,12 @@
 fetch <url>
 use urllib module
 """
-import urllib.request  # Importe le module urllib.request
+import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'  # Définit l'URL à récupérer
-
-with urllib.request.urlopen(url) as response:  # Ouvre une connexion avec l'URL
-    html = response.read()  # Lit le contenu de la réponse
-    print("Body response:")  # Affiche un titre
-    print("\t- type: {}".format(type(html)))  # Affiche le type du contenu
-    print("\t- content: {}".format(html))  # Affiche le contenu brut
+url = 'https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
     print("\t- utf8 content: {}".format(html.decode('utf-8'))) 
